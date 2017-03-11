c95a649f06c043abf6dacb27d36e489876b7bfe5
Lars Sørensen <lsar13@student.aau.dk>
2017-03-11 14:22:18 +0100

    Nolonger needed.

--- Assets/scripts/MoveUnitForward.cs
+++ Assets/scripts/MoveUnitForward.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class MoveUnitForward : MonoBehaviour {
 
-	Rigidbody rb;
-	public bool turn;
-
 	void Start () {
-		rb = GetComponent<Rigidbody>();	
 	}
 	
 	void Update () {
-		rb.AddForce ((turn ? Vector3.right : Vector3.left)*500 * Time.deltaTime);
 	}
 }
