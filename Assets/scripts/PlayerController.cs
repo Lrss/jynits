798cf23d0f60156e854b078bf116d2a652d56574
Lars Sørensen <lsar13@student.aau.dk>
2017-03-11 01:36:18 +0100

    3 different unit spawn keys

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour {
 		Down
 	}
 
-	public string[] inputkeys = new string[Enum.GetNames(typeof(MyEnum)).Length] ;
+	public string[] inputkeys = new string[5] ;
 	public GameObject unit;
 	List<GameObject> Spawners = new List<GameObject>();
 	int selectedSpawner = 0;
@@ -31,29 +31,37 @@ public class PlayerController : MonoBehaviour {
 
 		Spawners[selectedSpawner].GetComponent<Renderer> ().material.color = Color.green;
 	}
-	bool[] KeyPress = new bool[Enum.GetNames(typeof(MyEnum)).Length];
-	bool[] DeltaPress = new bool[Enum.GetNames(typeof(MyEnum)).Length];
+	bool[] KeyPress = new bool[5];
+	bool[] DeltaPress = new bool[5];
 	void Update () {
 		foreach (Buttons btn in Enum.GetValues(typeof(Buttons))){
-			KeyPress[btn] = Input.GetKey(inputkeys[btn]);
+			KeyPress[(int)btn] = Input.GetKey(inputkeys[(int)btn]);
 		}
 
-		if(KeyPress[Buttons.Spawn1] && !DeltaPress[Buttons.Spawn1]){
-			var gb = Instantiate(unit, Spawners[selectedSpawner].transform.position + Vector3.up*2, Quaternion.identity);
-			if (transform.position.x < 0) {
-				gb.GetComponent<Renderer> ().material.color = Color.yellow;
-				gb.GetComponent<MoveUnitForward> ().turn = true;
-			} else {
-				gb.GetComponent<Renderer> ().material.color = Color.blue;
-			}
+		if(KeyPress[(int)Buttons.Spawn1] && !DeltaPress[(int)Buttons.Spawn1]){
+			var gb1 = Instantiate(unit, Spawners[selectedSpawner].transform.position + Vector3.up*2, Quaternion.identity);
+			gb1.GetComponent<Renderer> ().material.color = Color.yellow;
+			if (transform.position.x < 0)
+				gb1.GetComponent<MoveUnitForward> ().turn = true;
 		}
-
-		if (KeyPress[Buttons.Up] && !DeltaPress[Buttons.Up]) {
+		if(KeyPress[(int)Buttons.Spawn2] && !DeltaPress[(int)Buttons.Spawn2]){
+			var gb2 = Instantiate(unit, Spawners[selectedSpawner].transform.position + Vector3.up*2, Quaternion.identity);
+			gb2.GetComponent<Renderer> ().material.color = Color.blue;
+			if (transform.position.x < 0)
+				gb2.GetComponent<MoveUnitForward> ().turn = true;
+		}
+		if(KeyPress[(int)Buttons.Spawn3] && !DeltaPress[(int)Buttons.Spawn3]){
+			var gb3 = Instantiate(unit, Spawners[selectedSpawner].transform.position + Vector3.up*2, Quaternion.identity);
+					gb3.GetComponent<Renderer> ().material.color = Color.cyan;
+			if (transform.position.x < 0)
+				gb3.GetComponent<MoveUnitForward> ().turn = true;
+		}
+		if (KeyPress[(int)Buttons.Up] && !DeltaPress[(int)Buttons.Up]) {
 			if (selectedSpawner < Spawners.Count-1 ) {
 				selectedSpawner++;
 			}
 		}
-		else if(KeyPress[Buttons.Down] && !DeltaPress[Buttons.Down]){
+		else if(KeyPress[(int)Buttons.Down] && !DeltaPress[(int)Buttons.Down]){
 			if (selectedSpawner > 0 ) {
 				selectedSpawner--;
 			}
@@ -64,7 +72,7 @@ public class PlayerController : MonoBehaviour {
 		}
 		deltaSelectedSpawner = selectedSpawner;
 		foreach (Buttons btn in Enum.GetValues(typeof(Buttons))){
-				DeltaPress[btn] = KeyPress[btn];
+			DeltaPress[(int)btn] = KeyPress[(int)btn];
 		}
 	}
 }
